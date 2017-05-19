101203441d5713e4da89d82fb2ef92a5cb762098
AdamH0461 <hodgkinson.adam@gmail.com>
2017-05-19 18:53:40 +0100

    added more comments

--- palindromeCheck.py
+++ palindromeCheck.py
@@ -1,9 +1,8 @@
-import stringReverse as sr
-origin = input("enter Shenanigans")
+import stringReverse as sr #imports code from string reversal project
+origin = input("enter Shenanigans") 
 reverse = sr.stringRev(origin)
 if origin == reverse:
     print (origin + " reversed is " + reverse +"\n palindrome confirm")
 else :
     print ("U GOT NO PALINDROME")
     print (origin + " reversed is " + reverse)
-
