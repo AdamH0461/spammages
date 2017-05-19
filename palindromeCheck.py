6f9ccd9e36cf777421e5b81947b1d93fc680d50a
AdamH0461 <hodgkinson.adam@gmail.com>
2017-05-19 21:01:48 +0100

    attempting run

--- palindromeCheck.py
+++ palindromeCheck.py
@@ -1,10 +1,10 @@
-#program to check if input is a palindrome (word which is the same backwards),
-#using my string reversal program made earlier
-import stringReverse as sr #imports code from string reversal project
+# program to check if input is a palindrome (word which is the same backwards),
+# using my string reversal program made earlier
+import stringReverse as sr  # imports code from string reversal project
 origin = input("enter Shenanigans")
 reverse = sr.stringRev(origin)
 if origin == reverse:
-    print (origin + " reversed is " + reverse +"\n palindrome confirm")
-else :
-    print ("U GOT NO PALINDROME")
-    print (origin + " reversed is " + reverse)
+    print(origin + " reversed is " + reverse + "\n palindrome confirm")
+else:
+    print("U GOT NO PALINDROME")
+    print(origin + " reversed is " + reverse)
--- pigLatin.py
+++ pigLatin.py
@@ -3,7 +3,7 @@ string = input("enter string")
 appends = []
 vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
 if string[0] not in vowels:
-    appends[0] = string[0]
+    appends.insert(0, [0])
     if string[1]:
         appends[1] = string[1]
 appends.append("-ay")
