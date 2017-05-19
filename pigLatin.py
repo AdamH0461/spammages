b19122ff99739d007dbdcda612eaff510ee9e9c9
AdamH0461 <hodgkinson.adam@gmail.com>
2017-05-19 20:53:44 +0100

    syntax

--- pigLatin.py
+++ pigLatin.py
@@ -1,3 +1,10 @@
-#program to take first constonant, move it to the end and add "ay"
+# program to take first constonant, move it to the end and add "ay"
 string = input("enter string")
-if string[0]
+appends = []
+vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+if string[0] not in vowels:
+    appends[0] = string[0]
+    if string[1]:
+        appends[1] = string[1]
+appends.append("-ay")
+print(appends)
